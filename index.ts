383a74ed4f6c4053282d805a78f38028c117c0e8
Evgenii Rubin <kubikus2009@yandex.ru>
2025-02-23 22:01:30 +0100

    fix code

--- index.ts
+++ index.ts
@@ -1,29 +1,17 @@
 import { HexletCode } from './src/HexletCode';
 
-interface TemplateType {
+interface Template {
   name: string;
   job: string;
   gender: 'm' | 'f';
 }
 
-const template: TemplateType = { name: 'rob', job: 'hexlet', gender: 'm' };
+const template: Template = { name: 'rob', job: 'hexlet', gender: 'm' };
 
-console.log("Example 1:");
-let form = HexletCode.formFor<TemplateType>(template, { method: 'post' }, (f) => {
+const form = HexletCode.formFor(template, { method: 'post' }, (f) => {
   f.input('name');
   f.input('job', { as: 'textarea' });
   f.submit();
 });
+
 console.log(form);
-
-try {
-  form = HexletCode.formFor<TemplateType>(template, { url: '/users' }, (f) => {
-    f.input('name');
-    f.input('job', { as: 'textarea' });
-    f.submit();
-  });
-  console.log(form); // This line should not be reached if error is thrown
-} catch (e) {
-  console.error("Error caught:", e.message);
-}
-
